ce2f0e80bbd56ed18f6b67ca8e8f93eff60f2cb4
sash04ek <a.ovcharov@gmail.com>
2017-03-06 08:48:26 +0300

    refactoring namespaces

--- module.php
+++ module.php
@@ -18,9 +18,9 @@
  * @package Modules
  */
 
-namespace Aurora\Modules;
+namespace Aurora\Modules\GoogleDrive;
 
-class GoogleDriveModule extends \Aurora\System\Module\AbstractModule
+class Module extends \Aurora\System\Module\AbstractModule
 {
 	protected static $sService = 'google';
 	
