9e77a6297a5f23da7a8632fe3aceafa495cb9d7a
nadine-shi <nadine@afterlogic.com>
2019-04-26 14:40:44 +0300

    License and other texts improving

--- Module.php
+++ Module.php
@@ -8,6 +8,8 @@
 namespace Aurora\Modules\GoogleDrive;
 
 /**
+ * Adds ability to work with Google Drive file storage inside Aurora Files module.
+ * 
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
